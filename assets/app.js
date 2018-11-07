97fe3d8b1c04c0824b3d3a603051dc6f58abc973
herisatry <herisatry@gmail.com>
2018-11-07 10:02:09 +0200

    gls function completed. next step populate the data into the objects

--- assets/app.js
+++ assets/app.js
@@ -66,128 +66,54 @@ function myfunc() {
     
     // GLS FUNCTION
 
-    // object literal holding data for option elements
-var Select_List_Data = {
-    
-    'choices': { // name of associated select box
+    $(document).ready(function(){
+      
+        //let's create arrays
+        var ITALIA = [
+          {display: "Dark chocolate", value: "4" }, 
+          {display: "Milk chocolate", value: "milk-chocolate" }, 
+          {display: "White chocolate", value: "white-chocolate" },
+          {display: "Gianduja chocolate", value: "gianduja-chocolate" }];
+          
+        var NORWAY = [
+          {display: "Broccoli", value: "broccoli" }, 
+          {display: "Cabbage", value: "cabbage" }, 
+          {display: "Carrot", value: "carrot" },
+          {display: "Cauliflower", value: "cauliflower" }];
+          
+        var PORTUGAL = [
+          {display: "Frozen yogurt", value: "frozen-yogurt" }, 
+          {display: "Booza", value: "booza" }, 
+          {display: "Frozen yogurt", value: "frozen-yogurt" },
+          {display: "Ice milk", value: "ice-milk" }];
         
-        // names match option values in controlling select box
-        js: {
-            text: ['Scrolling Divs', 'Tooltips', 'Rotate Images', 'Scrollers', 'Banner Rotator'],
-            value: ['scroll', 'tooltips', 'rotate', 'scrollers', 'banner']
-        },
-        php: {
-            text: ['Random Image', 'Form Class', 'Table Class', 'Order Form'],
-            value: ['random', 'form', 'table', 'order']
-        },
-        tuts: {
-            // example without values
-            text: ['Iframes', 'PHP to JS', 'Object Literals', 'Initializing JS']
-        }
-    
-    }    
-};
-
-// removes all option elements in select box 
-// removeGrp (optional) boolean to remove optgroups
-function removeAllOptions(sel, removeGrp) {
-    var len, groups, par;
-    if (removeGrp) {
-        groups = sel.getElementsByTagName('optgroup');
-        len = groups.length;
-        for (var i=len; i; i--) {
-            sel.removeChild( groups[i-1] );
-        }
-    }
-    
-    len = sel.options.length;
-    for (var i=len; i; i--) {
-        par = sel.options[i-1].parentNode;
-        par.removeChild( sel.options[i-1] );
-    }
-}
-
-function appendDataToSelect(sel, obj) {
-    var f = document.createDocumentFragment();
-    var labels = [], group, opts;
-    
-    function addOptions(obj) {
-        var f = document.createDocumentFragment();
-        var o;
-        
-        for (var i=0, len=obj.text.length; i<len; i++) {
-            o = document.createElement('option');
-            o.appendChild( document.createTextNode( obj.text[i] ) );
+        //If country option is changed
+        $("#country_selection").change(function() {
+            var country = $(this).val(); //get option value from country 
             
-            if ( obj.value ) {
-                o.value = obj.value[i];
-            }
-            
-            f.appendChild(o);
-        }
-        return f;
-    }
-    
-    if ( obj.text ) {
-        opts = addOptions(obj);
-        f.appendChild(opts);
-    } else {
-        for ( var prop in obj ) {
-            if ( obj.hasOwnProperty(prop) ) {
-                labels.push(prop);
-            }
+            switch(country){ //using switch compare selected option and populate child
+                case 'ITALIA':
+                 list(ITALIA);
+                break;
+                case 'NORWAY':
+                 list(NORWAY);
+                break;				
+                case 'PORTUGAL':
+                 list(PORTUGAL);
+                break;	
+              default: //default child option is blank
+                $("#blank_type").html('');	 
+                break;
+               }
+        });
+        
+        //function to populate child select box
+        function list(array_list)
+        {
+          $("#blank_type").html(""); //reset child options
+          $(array_list).each(function (i) { //populate child options 
+            $("#blank_type").append("<option value=\""+array_list[i].value+"\">"+array_list[i].display+"</option>");
+          });
         }
         
-        for (var i=0, len=labels.length; i<len; i++) {
-            group = document.createElement('optgroup');
-            group.label = labels[i];
-            f.appendChild(group);
-            opts = addOptions(obj[ labels[i] ] );
-            group.appendChild(opts);
-        }
-    }
-    sel.appendChild(f);
-}
-
-// anonymous function assigned to onchange event of controlling select box
-document.forms['glsForm'].elements['category'].onchange = function(e) {
-    // name of associated select box
-    var relName = 'choices';
-    
-    // reference to associated select box 
-    var relList = this.form.elements[ relName ];
-    
-    // get data from object literal based on selection in controlling select box (this.value)
-    var obj = Select_List_Data[ relName ][ this.value ];
-    
-    // remove current option elements
-    removeAllOptions(relList, true);
-    
-    // call function to add optgroup/option elements
-    // pass reference to associated select box and data for new options
-    appendDataToSelect(relList, obj);
-};
-
-
-// populate associated select box as page loads
-(function() { // immediate function to avoid globals
-    
-    var form = document.forms['glsForm'];
-    
-    // reference to controlling select box
-    var sel = form.elements['category'];
-    sel.selectedIndex = 0;
-    
-    // name of associated select box
-    var relName = 'choices';
-    // reference to associated select box
-    var rel = form.elements[ relName ];
-    
-    // get data for associated select box passing its name
-    // and value of selected in controlling select box
-    var data = Select_List_Data[ relName ][ sel.value ];
-    
-    // add options to associated select box
-    appendDataToSelect(rel, data);
-    
-}());
+        });
